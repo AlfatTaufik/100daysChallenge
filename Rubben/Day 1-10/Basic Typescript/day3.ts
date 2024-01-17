dd7de575c9a15a2b07fe29fdfba4e9e5b4c0854c
Ryxena <rubbenme@proton.me>
2024-01-17 21:08:35 +0700

    Day 3

---
+++ Rubben/Day 1-10/Basic Typescript/day3.ts
@@ -0,0 +1,11 @@
+// Conditional 
+function sayHello(nama:string) {
+    switch(nama) {
+        case "Rubben":
+            return "Hello Rubben"
+        default: 
+            return `Hello ${nama}`
+    }
+}
+if (10 > 2) console.log("10 lebih besar")
+80 >= 75 ? "Lulus" : "Coba lagi"
