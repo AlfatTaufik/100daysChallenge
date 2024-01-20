8853a6de3ecc400e0fdf33cb00ef828b4aba0c09
Alfathere <alfat601@smk.belajar.id>
2024-01-20 09:42:24 +0700

    Day 6

---
+++ Alphard/csharp/NullAbleContext/Program.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+
+using System.Collections.Generic;
+
+var fooList = new List<FooBar>
+{
+    new(Id: 1, Name: "Foo"),
+    new(Id: 2, Name: "Bar")
+};
+
+FooBar fooBar = fooList.Find(f => f.Name == "Bar")!;
+
+// The FooBar type definition for example.
+record FooBar(int Id, string Name);
