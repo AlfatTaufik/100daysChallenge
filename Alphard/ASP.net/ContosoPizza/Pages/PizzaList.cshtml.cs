b7e0da4cad9b99ec3cab7de56b3633c9f537fb3d
Alfathere <alfat601@smk.belajar.id>
2024-01-21 22:22:06 +0700

    Day 7

--- Alphard/ASP.net/ContosoPizza/Pages/PizzaList.cshtml.cs
+++ Alphard/ASP.net/ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -7,15 +7,39 @@ namespace ContosoPizza.Pages
 {
     public class PizzaListModel : PageModel
     {
+        public IActionResult OnPostDelete(int id)
+{
+    _service.DeletePizza(id);
+
+    return RedirectToAction("Get");
+}
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList {get;set;} = default!;
         public PizzaListModel(PizzaService service)
         {
             _service = service;
+
+            
         }
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
         }
+        [BindProperty]
+        public Pizza NewPizza {get;set;} = default!;
+
+        public IActionResult OnPost()
+{
+    if (!ModelState.IsValid || NewPizza == null)
+    {
+        return Page();
     }
+
+    _service.AddPizza(NewPizza);
+
+    return RedirectToAction("Get");
+}
+    }
+
+    
 }
