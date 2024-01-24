e36b23e9c132400fd7b88d7076b14731ba2f6167
Alfathere <alfat601@smk.belajar.id>
2024-01-24 23:16:48 +0700

    day 10

--- Alphard/csharp/csharp_0/Program.cs
+++ Alphard/csharp/csharp_0/Program.cs
@@ -7,5 +7,19 @@ string status = "   Gfriend    ";
 status = status.Trim();
 secondFriend = secondFriend.TrimEnd();
 firstFriend = firstFriend.TrimStart();    
+string friends = $"I have two friends that is {firstFriend} and {secondFriend} and {secondFriend} is My {status}";
 
-Console.WriteLine($"I have two friends that is {firstFriend} and {secondFriend} and {secondFriend} is My {status}");
+// Ini gaakan create new instance dan value default masih maria
+Console.WriteLine(friends.Replace("Maria", "Ikhsan"));
+// Create new instance
+friends = friends.Replace("Maria", "Ikhsan");
+Console.WriteLine($"{friends}");
+
+string apakah = friends.Contains("Ikhsan") ? "Yes" : "No";
+string pacar = friends.Contains("Hania") ? "yes" : "No";
+Console.WriteLine($"ada ikhsan {apakah} ada Hania {pacar}");
+Console.WriteLine($"{secondFriend.ToUpper()} {firstFriend.ToLower()}");
+Console.WriteLine($"Jumlah huruf {secondFriend.Length}");
+// Cek true and false significant white space
+
+Console.WriteLine($"{firstFriend.StartsWith("Ma")}");
