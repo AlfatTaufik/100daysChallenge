f5a340afee552f10b4e2c9075b1cee2f1eec3e64
Alfathere <alfat601@smk.belajar.id>
2024-01-30 21:31:53 +0700

    Day 16

---
+++ Alphard/codewars C#/ArrayDiff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+public class ArrayDiff
+{
+  public static void Main()
+  {
+    string[] buah = { "Apel", "Jeruk", "Mangga", "Anggur", "Pisang", "Jambu" };
+    string[] buah2 = { "Apel", "Jeruk", "Nanas", "Pisang" };
+    // Balikin balikin = new Balikin();
+    // Console.WriteLine($"{balikin()}")
+    string[] hasil = Balikin.Tuker(buah, buah2);
+    // Console.WriteLine($"{hasil}");
+    foreach (var satu in hasil)
+    {
+      Console.WriteLine(satu);
+    }
+  }
+}
+// string[] fruits = {""}
+
+public class Balikin
+{
+  public static string[] Tuker(string[] a, string[] b)
+  {
+    return a.Where(a => !b.Contains(a)).ToArray();
+  }
+}
