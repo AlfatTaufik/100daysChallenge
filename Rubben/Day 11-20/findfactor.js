8b5b2967b816a78061f78ccfd71166bd9c092cd8
Ryxena <rubbenme@proton.me>
2024-02-02 23:00:14 +0700

    Day 19

---
+++ Rubben/Day 11-20/findfactor.js
@@ -0,0 +1,13 @@
+function findFactors(n) {
+    let factors = [];
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        factors.push(i);
+        if (i !== n / i) {
+          factors.push(n / i);
+        }
+      }
+    }
+    return factors.sort((a, b) => a - b);
+  }
+console.info(findFactors(32))
