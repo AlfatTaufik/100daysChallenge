2ab19524dec4b80f4fa90b9b65cb12bbc8e95a1c
Ryxena <rubbenme@proton.me>
2024-02-04 14:16:35 +0700

    day 21

---
+++ Rubben/day 21-30/2sum.js
@@ -0,0 +1,7 @@
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}
