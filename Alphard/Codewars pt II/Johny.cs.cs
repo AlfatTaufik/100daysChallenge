813316687f3a0c73b10f953613a48d5d96baa774
Alfathere <alfat601@smk.belajar.id>
2024-02-11 23:09:58 +0700

    Day 28

---
+++ Alphard/Codewars pt II/Johny.cs.cs
@@ -0,0 +1,14 @@
+
+
+public static class Kata
+{
+  public static string greet(string name)
+  {
+    if(name == "Johnny"){
+      return "Hello, my love!";
+    }
+    else {
+      return "Hello, " + name + "!";
+    }
+  }
+}
